3dac6d2328939955ceee70d75601b9918877e751
Alessandro Festa <47183399+alfsuse@users.noreply.github.com>
2020-09-24 18:09:06 +0200

    Update demo.py

--- demo03/demo.py
+++ demo03/demo.py
@@ -1,107 +1,19 @@
-# Import data
-import time
-import numpy as np
+import os
+import json
+from tensorflow.python.lib.io import file_io as file_io
 
-from skimage import io
+static_html_path = os.path.join('/output/', 'demo.html')
+file_io.write_string_to_file(static_html_path, rendered_template)
 
-from PIL import Image, ImageSequence 
-from Pillow import gifmaker
-
-
-vol = io.imread("https://s3.amazonaws.com/assets.datacamp.com/blog_assets/attention-mri.tif")
-volume = vol.T
-r, c = volume[0].shape
-
-# Define frames
-import plotly.graph_objects as go
-nb_frames = 68
-
-fig = go.Figure(frames=[go.Frame(data=go.Surface(
-    z=(6.7 - k * 0.1) * np.ones((r, c)),
-    surfacecolor=np.flipud(volume[67 - k]),
-    cmin=0, cmax=200
-    ),
-    name=str(k) # you need to name the frame for the animation to behave properly
-    )
-    for k in range(nb_frames)])
-
-# Add data to be displayed before animation starts
-fig.add_trace(go.Surface(
-    z=6.7 * np.ones((r, c)),
-    surfacecolor=np.flipud(volume[67]),
-    colorscale='Gray',
-    cmin=0, cmax=200,
-    colorbar=dict(thickness=20, ticklen=4)
-    ))
-
-
-def frame_args(duration):
-    return {
-            "frame": {"duration": duration},
-            "mode": "immediate",
-            "fromcurrent": True,
-            "transition": {"duration": duration, "easing": "linear"},
-        }
-
-sliders = [
-            {
-                "pad": {"b": 10, "t": 60},
-                "len": 0.9,
-                "x": 0.1,
-                "y": 0,
-                "steps": [
-                    {
-                        "args": [[f.name], frame_args(0)],
-                        "label": str(k),
-                        "method": "animate",
-                    }
-                    for k, f in enumerate(fig.frames)
-                ],
-            }
-        ]
-
-# Layout
-fig.update_layout(
-         title='Slices in volumetric data',
-         width=600,
-         height=600,
-         scene=dict(
-                    zaxis=dict(range=[-0.1, 6.8], autorange=False),
-                    aspectratio=dict(x=1, y=1, z=1),
-                    ),
-         updatemenus = [
-            {
-                "buttons": [
-                    {
-                        "args": [None, frame_args(50)],
-                        "label": "&#9654;", # play symbol
-                        "method": "animate",
-                    },
-                    {
-                        "args": [[None], frame_args(0)],
-                        "label": "&#9724;", # pause symbol
-                        "method": "animate",
-                    },
-                ],
-                "direction": "left",
-                "pad": {"r": 10, "t": 70},
-                "type": "buttons",
-                "x": 0.1,
-                "y": 0,
-            }
-         ],
-         sliders=sliders
-)
-
-im = fig.show()
-sequence = []
-
-im = Image.open(im)
-
-# im is your original image
-frames = [frame.copy() for frame in ImageSequence.Iterator(im)]
-
-# write GIF animation
-fp = open("/output/out.gif", "wb")
-gifmaker.makedelta(fp, frames)
-fp.close()
+metadata = {
+'outputs' : [{
+    'type': 'web-app',
+    'storage': 'inline',
+    'source': '/output/demo.html',
+}, {
+    'type': 'web-app',
+    'storage': 'inline',
+}]
+}
+with file_io.FileIO('/mlpipeline-ui-metadata.json', 'w') as f:
+    json.dump(metadata, f)
